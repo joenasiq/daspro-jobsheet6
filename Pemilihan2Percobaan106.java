220ff6f0e20cc612515a32c64d8b082bb9f7c9e4
joenasiq <arjunash2006@gmail.com>
2024-10-08 21:00:12 +0700

    else if

--- Pemilihan2Percobaan106.java
+++ Pemilihan2Percobaan106.java
@@ -4,13 +4,14 @@ public class Pemilihan2Percobaan106 {
         Scanner sc = new Scanner(System.in);
         System.out.print("masukkan tahun: ");
         int tahun = sc.nextInt();
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0 || (tahun % 400) == 0) {
-            System.out.println("tahun kabisat");
-            } 
-            else {
-            System.out.println("bukan tahun kabisat");
-            }
+        if (tahun % 400 == 0) {
+            System.out.println("Tahun kabisat");
+        } else if (tahun % 100 == 0) {
+            System.out.println("Bukan tahun kabisat");
+        } else if (tahun % 4 == 0) {
+            System.out.println("Tahun kabisat");
+        } else {
+            System.out.println("Bukan tahun kabisat");
         }
     }
 }
